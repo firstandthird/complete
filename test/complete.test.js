fc6c60207638b3f4c6565516563f502c8dd2f439
Antonio Laguna <antonio.matias@gamesys.co.uk>
2013-07-03 16:55:04 +0100

    Improving the tests
    Finished init tests

--- test/complete.test.js
+++ test/complete.test.js
@@ -1,18 +1,21 @@
+var autocompleteSource = [
+  'Acura', 'Audi', 'BMW', 'Cadillac',
+  'Chrysler', 'Dodge', 'Ferrari', 'Ford',
+  'GMC', 'Honda', 'Hyundai', 'Infiniti',
+  'Jeep', 'Kia', 'Lexus', 'Mini',
+  'Nissan', 'Porsche', 'Subaru', 'Toyota',
+  'Volkswagon', 'Volvo'
+];
+
 
 suite('complete', function() {
-  var autocomplete;
+  var autocomplete, fidelAutocomplete;
 
   setup(function(){
     autocomplete = $('#autocomplete').autocomplete({
-      source : [
-        'Acura', 'Audi', 'BMW', 'Cadillac',
-        'Chrysler', 'Dodge', 'Ferrari', 'Ford',
-        'GMC', 'Honda', 'Hyundai', 'Infiniti',
-        'Jeep', 'Kia', 'Lexus', 'Mini',
-        'Nissan', 'Porsche', 'Subaru', 'Toyota',
-        'Volkswagon', 'Volvo'
-      ]
+      source : autocompleteSource
     });
+    fidelAutocomplete = autocomplete.data('autocomplete');
   });
 
   suite('init',function(){
@@ -20,9 +23,14 @@ suite('complete', function() {
       assert.equal(typeof $().autocomplete, 'function');
     });
     test('autocomplete should create a div in which results will be presented', function(){
+      var div = autocomplete.next();
+      assert.equal(div.hasClass(fidelAutocomplete.listClass), true);
     });
     test('autocomplete should turn browser autocompletion off', function(){
       assert.equal(autocomplete.attr('autocomplete'), 'off');
+    });
+    test('autocomplete should store internal sources', function(){
+      assert.equal(autocompleteSource.length, fidelAutocomplete.source.length);
     });
   });
 });
