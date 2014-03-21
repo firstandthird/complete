00f6f270d9dbba840b92d11ff336c152a93c3c2b
Antonio Laguna <antonio@potatolondon.com>
2014-03-21 22:14:09 +0000

    Switch to mixin
    Fixes #12

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,6 +74,13 @@ module.exports = function(grunt) {
           livereload: true
         }
       },
+      styles: {
+        files: 'less/*.less',
+        tasks: 'less',
+        options: {
+          livereload: true
+        }
+      },
       example: {
         files: [
           'example/*'
@@ -127,6 +134,9 @@ module.exports = function(grunt) {
           'dist/*'
         ]
       }
+    },
+    less: {
+      'dist/complete.css' : 'less/default.less'
     }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -135,13 +145,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-concat-bower');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-bytesize');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-plato');
   grunt.registerTask('scripts', ['jshint', 'bower', 'concat', 'uglify', 'clean:bower', 'mocha', 'bytesize']);
-  grunt.registerTask('default', ['scripts']);
-  grunt.registerTask('dev', ['connect:server', 'watch']);
+  grunt.registerTask('default', ['scripts', 'less']);
+  grunt.registerTask('dev', ['default', 'connect:server', 'watch']);
   grunt.registerTask('reports', ['plato', 'connect:plato']);
 };
