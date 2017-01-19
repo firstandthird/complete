c859a172fe8a03c2c86a6d2ced58b102fb19b897
Troy Whiteley <troy@somany.us>
2017-01-19 15:20:59 -0800

    Update tests

--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ Example html
 ```html
 <div data-module="Complete" data-module-endpoint="/api/autocomplete?q=${term}">
   <input type="text" data-name="input" data-action="search" data-action-type="input" placeholder="Search for something">
-  <div data-name="results-container">
+  <div data-name="resultsContainer">
     <ul data-name="results"></ul>
   </div>
 </div>
--- test/complete.test.js
+++ test/complete.test.js
@@ -19,7 +19,7 @@ const setup = () => {
   container.innerHTML = `
     <div data-module="Complete" data-module-endpoint="${testEndpoint}">
       <input type="text" data-name="input" data-action="search" data-action-type="input" placeholder="Search for something">
-      <div data-name="results-container">
+      <div data-name="resultsContainer">
         <ul data-name="results"></ul>
       </div>
     </div>
@@ -41,8 +41,31 @@ test('search', assert => {
   const instance = modules[0];
 
   setTimeout(() => {
+    const elements = document.querySelectorAll('li');
+    assert.equal(elements.length, 2, 'Correct number of elements rendered');
+    assert.equal(elements[0].innerHTML, 'Test 1', 'First element correct');
+    assert.equal(elements[0].dataset.action, 'select', 'Action setup');
+    assert.equal(elements[0].dataset.actionValue, 'test1', 'Action value setup');
     assert.end();
-  }, 500);
+  }, 100);
+
+  instance.els.input.focus();
+  page.sendEvent('keypress', 'a');
+});
+
+test('select', assert => {
+  const modules = setup();
+  const instance = modules[0];
+
+  setTimeout(() => {
+    const elements = document.querySelectorAll('li');
+    const pos = elements[0].getBoundingClientRect();
+
+    page.sendEvent('click', pos.left + pos.width / 2, pos.top + pos.height / 2);
+
+    assert.equal(instance.els.input.value, 'test1', 'Value selected');
+    assert.end();
+  }, 100);
 
   instance.els.input.focus();
   page.sendEvent('keypress', 'a');
