17b16223f7c4462177292553502ec8ad3a83753a
Troy Whiteley <troy@somany.us>
2017-01-19 13:46:43 -0800

    WIP - Modernize

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 Example html
 
 ```html
-<div data-module="complete" data-module-endpoint="/api/autocomplete?q=${term}">
+<div data-module="Complete" data-module-endpoint="/api/autocomplete?q=${term}">
   <input type="text" data-name="input" data-action="search" data-action-type="input" placeholder="Search for something">
   <div data-name="results-container">
     <ul data-name="results"></ul>
--- test/complete.test.js
+++ test/complete.test.js
@@ -1,8 +1,49 @@
-import Complete from '../lib/complete';
+
+import 'core-js';
+import Domodule from '../lib/complete';
 
 import { test } from 'tape';
 
-test('implementation', assert => {
-  assert.equal(typeof Complete, 'function', 'Complete class exists');
+const testEndpoint = './test/autocomplete.json';
+
+const page = window.phantom.page;
+
+const init = () => {
+  const container = document.createElement('div');
+  container.id = 'domodule';
+  document.body.appendChild(container);
+};
+
+const setup = () => {
+  const container = document.getElementById('domodule');
+  container.innerHTML = `
+    <div data-module="Complete" data-module-endpoint="${testEndpoint}">
+      <input type="text" data-name="input" data-action="search" data-action-type="input" placeholder="Search for something">
+      <div data-name="results-container">
+        <ul data-name="results"></ul>
+      </div>
+    </div>
+  `;
+  const modules = Domodule.discover();
+  return modules;
+};
+
+init();
+
+test('example module registerd', assert => {
+  assert.equal(typeof Domodule.modules, 'object');
+  assert.equal(Object.keys(Domodule.modules).length, 1, 'one module registered');
   assert.end();
+});
+
+test('search', assert => {
+  const modules = setup();
+  const instance = modules[0];
+
+  setTimeout(() => {
+    assert.end();
+  }, 500);
+
+  instance.els.input.focus();
+  page.sendEvent('keypress', 'a');
 });
